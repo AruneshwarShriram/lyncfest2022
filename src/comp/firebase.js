ab8036711ccad33acda7060cefea7beb64791549
AruneshwarShriram <shri13062002@gmail.com>
2022-09-22 09:03:24 +0530

    first commit

--- src/comp/firebase.js
+++ src/comp/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDRhNJ4ugmIbulmsFkJEPqr8P5zrQmDy-8",
-  authDomain: "lyncfest2022.firebaseapp.com",
-  projectId: "lyncfest2022",
-  storageBucket: "lyncfest2022.appspot.com",
-  messagingSenderId: "651207500362",
-  appId: "1:651207500362:web:cd8c04cf93d31463207cb3",
-  measurementId: "G-CK5BB9J0J0"
+    apiKey: "AIzaSyDRhNJ4ugmIbulmsFkJEPqr8P5zrQmDy-8",
+    authDomain: "lyncfest2022.firebaseapp.com",
+    projectId: "lyncfest2022",
+    storageBucket: "lyncfest2022.appspot.com",
+    messagingSenderId: "651207500362",
+    appId: "1:651207500362:web:cd8c04cf93d31463207cb3",
+    measurementId: "G-CK5BB9J0J0"
 };
 
 // Initialize Firebase
